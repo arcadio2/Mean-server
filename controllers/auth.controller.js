5eca2e5f9236824f130102935c4557befd5ba0b4
Arkdio <arcadiolg2@gmail.com>
2021-08-21 01:52:08 -0500

    Añadidos JWT

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const {response} = require('express'); 
 const Usuario = require('../models/Usuario');
 const bcrypt = require('bcryptjs');
+const { generarJWT } = require('../helpers/jwt');
 
 
 const crearUsuario = async (req, res=response)=>{
@@ -24,7 +25,7 @@ const crearUsuario = async (req, res=response)=>{
 
 
         //Generar JWT
-    
+        const token = await generarJWT(dbUser.id,name); 
 
         //Crear usuario de DB 
         await dbUser.save(); 
@@ -33,7 +34,8 @@ const crearUsuario = async (req, res=response)=>{
         return res.status(201).json({
             ok:true,
             uid:dbUser.id,
-            name
+            name, 
+            token
         }); 
 
     }catch(error){
@@ -45,19 +47,56 @@ const crearUsuario = async (req, res=response)=>{
     }
 }
 
-const loginUsuario = (req, res=response)=>{
+const loginUsuario = async (req, res=response)=>{
     const {email, password} = req.body; 
-    
+    try{
+
+        const dbUser = await Usuario.findOne({email})
+        if(!dbUser ){
+            return res.status(400).json({
+                ok:false,
+                msg:'Las credenciales no son válidas'
+            }); 
+        }
+        const validPassword = bcrypt.compareSync(password,dbUser.password);
+        if(!validPassword){
+            return res.status(400).json({
+                ok:false,
+                msg:'Las credenciales no son válidas'
+            }); 
+        }
+
+        //Generar JWT
+        const token = await generarJWT(dbUser.id, dbUser.name); 
+
+        //Respuesta correcta
+        return res.json({
+            ok:true,
+            uid:dbUser.id,
+            name: dbUser.name, 
+            token
+        })
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            ok:false,
+            msg: 'Hable con el administrador'
+        }); 
+    }
     return res.json({
         ok:true,
         msg:'Login usuario /new'
     })
 }
 
-const revalidarToken = (req, res)=>{
+const revalidarToken = async (req, res)=>{
+    const {uid,name} = req; 
+    const token = await generarJWT(uid,name);
     return res.json({
         ok:true,
-        msg:'Renew'
+        uid,
+        name,
+        token
     })
 }
 
